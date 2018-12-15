dadaa9c31e292e34489402559a719e7c0ebb0e23
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2018-12-15 23:01:36 +0100

    :sparkles: Passing data from post-list to post-list-item : one post of "posts" array

--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post-list-item',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list-item.component.css']
 })
 export class PostListItemComponent implements OnInit {
+
+  @Input() post: Object;
 
   constructor() { }
 
